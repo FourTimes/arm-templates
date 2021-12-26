3bd1b69f166fd636f3e294f756dbe46c04fdd87b
Your Name <you@example.com>
2021-12-26 11:32:50 +0530

    modified

--- demo.sh
+++ demo.sh
@@ -1,1 +1,6 @@
-az deployment group create --name Azure_GCP_Migrations --resource-group Azure_GCP_Migration_Adds --template-file template.json --parameters parameters.json
+# RewriteEngine on
+# rewritemap lowercase int:tolower
+# RewriteCond $1 [A-Z]
+# RewriteRule ^/(.*)$ /${lowercase:$1} [R=301,L]
+
+az deployment group create --name Azure_GCP_Migration --resource-group Azure_GCP_Migration_Adds --template-file template.json --parameters parameters.json
